37b06c62f9bdb91a4d1e01a6253a238e1b84b1e2
Kateryna Skoryna <k.skoryna@gmail.com>
2026-08-05 11:12:39 +0200

    Add shared todo validation schemas

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/todo.types';
 export * from './lib/auth.types';
 export * from './lib/validation';
 export * from './lib/schemas';
+export * from './lib/todo.schemas';
+export * from './lib/todolist.schemas';
